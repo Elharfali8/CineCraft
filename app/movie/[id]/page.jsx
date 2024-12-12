ff300b2964aa3d9edc3397e6dee423fe5097de12
Elharfali <0631173826you@gmail.com>
2024-12-12 20:39:58 +0100

    Implement Movies and Tv shows pages

---
+++ app/movie/[id]/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SinglePage = () => {
+  return (
+      <div className='mt-20 min-h-[calc(100vh-80px)]'>
+          <div className="container main-container">
+              
+          </div>
+    </div>
+  )
+}
+
+export default SinglePage
